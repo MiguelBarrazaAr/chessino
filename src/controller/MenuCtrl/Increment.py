253d5f859e88837e8a130437df0b3c5018613f42
Miguel Barraza <miguel.barraza@mercadolibre.com>
2022-11-28 22:06:48 -0300

    fix corregiido incremento

--- src/controller/MenuCtrl/Increment.py
+++ src/controller/MenuCtrl/Increment.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from .ItemSwitch import ItemSwitch
 
-class Sounds(ItemSwitch):
-    def Increment(self):
+class Increment(ItemSwitch):
+    def start(self):
         self.audio = "incremento"
         self.setParam('add-increment')
